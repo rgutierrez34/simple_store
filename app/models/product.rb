73022ae41ad521b15677bee230d075eaf0cc071f
Ron Gutierrez <gutierrez_ron@hotmail.com>
2014-10-31 21:32:30 -0500

    Final Commit Before Submission

---
+++ app/models/product.rb
@@ -0,0 +1,4 @@
+class Product < ActiveRecord::Base
+  validates :title, :price, :stock_quantity, presence: true
+  attr_accessible :description, :price, :stock_quantity, :title
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleStore::Application.config.secret_token = 'd9783f9a35b6a55104d5846516bdfc96a00b953971859f90d92babcfa7eb1ad8c70f3d3bcfab043a65845cb07373455564b299da87ae8973f0459363fd2200e2'
